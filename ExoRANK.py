881ff4eca03ceecb75235271b87c34c110c4e950
Hunter Brooks <85brookshunter85@gmail.com>
2024-06-11 13:04:54 -0700

    Beta v0.2.0

--- ExoRANK.py
+++ ExoRANK.py
@@ -1,5 +1,5 @@
 #-----------------------------------------------------------------------#
-# ExoRANK v?
+# ExoRANK v0.2.0
 # By Hunter Brooks, at NAU, Flagstaff: June 10, 2024
 #
 # Purpose: Rank a Table Based Off Given Parameters
@@ -8,114 +8,203 @@
 
 # Import all needed packages.
 # ------------------------------------------------------------- #
-# GUI Packages
-import PySimpleGUI as sg
+import numpy as np
 import pandas as pd
+from tqdm import tqdm
+import PySimpleGUI as sg
 from astropy.io import fits
 from astropy.io import ascii
-from tqdm import tqdm
-import numpy as np
-import csv
+# ------------------------------------------------------------- #
 
-def table_read(): 
-    #Reads in the file depending on the filetype
-    if values['type'] == 'CSV':
-        table = pd.read_csv(values['file'])
-    if values['type'] == 'FITS':
-        fits_table = fits.open(values['file'])
-        table = fits_table[1].data
-    if values['type'] == 'ASCII':
-        table = ascii.read(values['file'])
-    if values['type'] == 'IPAC':
-        table = ascii.read(values['file'], format = 'ipac')
+
+# Table Reading Functions
+# ------------------------------------------------------------- #
+def table_read():
+    try:
+        # Reads in the file depending on the filetype
+        if values['type'] == 'CSV':
+            # If the file is a CSV, read it using pandas
+            table = pd.read_csv(values['file'])
+        if values['type'] == 'FITS':
+            # If the file is a FITS, read it using astropy
+            fits_table = fits.open(values['file'])
+            table = fits_table[1].data
+        if values['type'] == 'ASCII':
+            # If the file is an ASCII file, read it using astropy's ascii module
+            table = ascii.read(values['file'])
+        if values['type'] == 'IPAC':
+            # If the file is an IPAC table, read it using astropy's ascii module with IPAC format
+            table = ascii.read(values['file'], format='ipac')
+        
+        # Print confirmation message that the table has been loaded
+        print('')
+        print('#           Table Has Been Loaded!             #')
+        print('')
+        
+        # Return the loaded table
+        return table
     
-    print('')
-    print('#           Table Has Been Loaded!             #')
-    print('')
-    return table
+    except:
+        # In case of an error, return 0
+        return 0
 
-def column_read(table, user_options, user_types): 
-    total_column = []
-    for i in range(len(user_options)): 
-        total_column.append(table[user_options[i]].tolist())
-    return total_column
+def column_read(table, user_options, user_types):
+    try:
+        # Initialize an empty list to hold the column data
+        total_column = []
+        
+        # Iterate over the list of user-specified columns
+        for i in range(len(user_options)):
+            # Append the data from the specified column to the total_column list
+            total_column.append(table[user_options[i]].tolist())
+        
+        # Return the list of columns
+        return total_column
+    
+    except:
+        # In case of an error, return 0
+        return 0
+# ------------------------------------------------------------- #    
 
-def parm_metrix(column_space, user_types): 
+
+
+# Matrix Creation Functions
+# ------------------------------------------------------------- #
+def parm_metrix(column_space, user_types):
+    # Initialize an empty list to hold the total parameter space
     total_parm_space = []
     
+    # Print a message indicating that the parameter space is being read
     print('')
     print('#           Parameter Space is Being Read!             #')
     
-    for i in tqdm(range(len(column_space[0]))): 
+    # Loop over each entry in the first column of column_space
+    for i in tqdm(range(len(column_space[0]))):
+        # Initialize a temporary list to hold the current parameter space values
         temp_parm_space = []
-        for j in range(len(user_types)): 
+        
+        # Loop over each user-defined type
+        for j in range(len(user_types)):
             current_type = user_types[j]
-            if current_type == 'pwd': 
-                temp_value = 1
+            
+            # Check the type and calculate the corresponding parameter value
+            if current_type == 'pwd':
+                # Convert to pwd (assumed to be milliparsecs)
+                temp_value = (column_space[j][i]) * 1000
                 temp_parm_space.append(temp_value)
-            if current_type == 'mag': 
-                temp_value = 1
+            if current_type == 'mag':
+                # Convert to magnitude
+                temp_value = 1000 / (column_space[j][i])
                 temp_parm_space.append(temp_value)
-            if current_type == 'plx': 
-                temp_value = 1
+            if current_type == 'plx':
+                # Convert to parallax (assumed to be in milliarcseconds)
+                temp_value = (column_space[j][i]) / 100
                 temp_parm_space.append(temp_value)
-            if current_type == 'distance': 
-                temp_value = 1
+            if current_type == 'distance':
+                # Convert to distance in parsecs
+                temp_value = (1000 / (column_space[j][i])) / 100
                 temp_parm_space.append(temp_value)
-            if current_type == 'teff': 
-                temp_value = 1
+            if current_type == 'teff':
+                # Convert to effective temperature (assumed to be in units of 10,000 K)
+                temp_value = (column_space[j][i]) / 10000
                 temp_parm_space.append(temp_value)
-            if current_type == 'pm': 
-                temp_value = 1
+            if current_type == 'pm':
+                # Convert to proper motion (assumed to be in milliarcseconds/year)
+                temp_value = 100 / (column_space[j][i])
                 temp_parm_space.append(temp_value)
-            if current_type == 'spt': 
-                temp_value = 1
-                temp_parm_space.append(temp_value)
+        
+        # Append the temporary parameter space list to the total parameter space list
         total_parm_space.append(temp_parm_space)
         
+    # Print a message indicating that the parameter space has been read
     print('#           Parameter Space Has Been Read!             #')
     print('')
+    
+    # Return the total parameter space list
     return total_parm_space
 
+# ------------------------------------------------------------- #
+
+
+
+# Matrix Ranking Functions
+# ------------------------------------------------------------- #
 def ranking_mult(parameter_matrix, user_scalings):
+    # Initialize an empty list to hold the total rank list
     total_rank_list = []
+    
+    # Print a message indicating that the table is being ranked
     print('')
     print('#           Table Is Being Ranked!             #')
     
-    for i in tqdm(range(len(parameter_matrix))): 
+    # Loop over each entry in the parameter matrix
+    for i in tqdm(range(len(parameter_matrix))):
+        # Get the current parameter space (row) from the parameter matrix
         temp_parm_space = parameter_matrix[i]
+        
+        # Initialize a temporary list to hold the rank values
         temp_rank = []
-        for j in range(len(temp_parm_space)): 
+        
+        # Loop over each parameter and apply the corresponding scaling factor
+        for j in range(len(temp_parm_space)):
             temp_rank.append(float(temp_parm_space[j]) * float(user_scalings[j]))  # Convert to float
+        
+        # Sum the scaled parameters and append to the total rank list
         total_rank_list.append(np.nansum(temp_rank))
         
+    # Print a message indicating that the table has been ranked
     print('#           Table Has Been Ranked!             #')
     print('')
+    
+    # Return the total rank list
     return total_rank_list
 
 def rank_table(table, ranked_list):
+    # Print a message indicating that ranks are being added to the table
     print('#      Ranks Are Being Added to Table!         #')
     print('')
-
+    
+    # Add the ranked list as a new column 'Rank' to the table
     table['Rank'] = ranked_list
+    
+    # Return the updated table
     return table
+# ------------------------------------------------------------- #
 
-def rank_table_save(ranked_table, output): 
+
+
+# Save Final Table Function
+# ------------------------------------------------------------- #
+def rank_table_save(ranked_table, output):
+    # Print a message indicating that the table is being saved
     print('')
     print('#           Table Is Being Saved!             #')
     print('')
     
+    # Convert the ranked table to a pandas DataFrame
     df = pd.DataFrame(ranked_table)
+    
+    # Save the DataFrame to a CSV file in the 'Output' directory with the specified output filename
     df.to_csv(f'Output/{output}.csv', index=False)
+# ------------------------------------------------------------- #
 
-# Sets General WRAP Theme
+
+
+# Sets General ExoRANK Settings
+# ------------------------------------------------------------- #
 sg.theme('DarkTeal1')
 
 #Makes the drop down window for types of file in the multi-object search
 filetype_list = ['CSV', 'FITS', 'ASCII', 'IPAC']
-option_type_list = ['pwd', 'mag', 'plx', 'distance', 'teff', 'pm', 'spt']
+option_type_list = ['pwd', 'mag', 'plx', 'distance', 'teff', 'pm']
+user_options = []
+user_scalings = []
+user_types = []
+# ------------------------------------------------------------- #
 
-# WRAP GUI LAYOUT
+
+
+# ExoRANK GUI Layout
 # ------------------------------------------------------------- #
 #Makes the layout of WRAP for the single object search, by providing a location for: ra, dec, radius, output file name, catalogs, and output
 layout = [
@@ -138,41 +227,46 @@ layout = [
 
 #Generates the window based off the layouts above
 window = sg.Window('ExoRANK', layout, size = (300, 380), grab_anywhere=False, finalize=True, enable_close_attempted_event = True)
-
-user_options = []
-user_scalings = []
-user_types = []
-
-# RUNNING WRAP GUI
 # ------------------------------------------------------------- #
-#Keeps the window open
+
+
+
+# Running ExoRANK GUI
+# ------------------------------------------------------------- #
 while True:
     #Reads all of the events and values, then reads which tab is currently in
     event, values = window.read()
     
     if event == "Change Settings":
-        try: 
+        try:
+            # Attempt to convert the number of options to an integer
             num_options = int(values['nop'])
-        except: 
+        except:
+            # Notify the user if there's an error in input
             print('#------------------------------------------------#')
             print('#   Please Enter a Correct Output!  #')
             print('#------------------------------------------------#')
             pass
-        col_option = [sg.Text('Column Name: ', font = ('Times New Roman', 22), size=(16, 1), justification='center')]
-        col_type = [sg.Text('Column Type: ', font = ('Times New Roman', 22), size=(16, 1), justification='center')]
-        col_scaling = [sg.Text('Column Scaling: ', font = ('Times New Roman', 22), size=(16, 1), justification='center')]
-        for i in range(num_options): 
-            col_option.append(sg.InputText(size = (18), font = ('Times New Roman', 15), key = f'option_{i}'))
-            col_type.append(sg.Combo(option_type_list, size = (16), font = ('Times New Roman', 15), key = f'type_{i}'))
-            col_scaling.append(sg.InputText(size = (18), font = ('Times New Roman', 15), key = f'scaling_{i}'))
+        
+        # Initialize lists for column options, types, and scalings
+        col_option = [sg.Text('Column Name: ', font=('Times New Roman', 22), size=(16, 1), justification='center')]
+        col_type = [sg.Text('Column Type: ', font=('Times New Roman', 22), size=(16, 1), justification='center')]
+        col_scaling = [sg.Text('Column Scaling: ', font=('Times New Roman', 22), size=(16, 1), justification='center')]
+        
+        # Generate input fields for each option based on the number of options
+        for i in range(num_options):
+            col_option.append(sg.InputText(size=(18), font=('Times New Roman', 15), key=f'option_{i}'))
+            col_type.append(sg.Combo(option_type_list, size=(16), font=('Times New Roman', 15), key=f'type_{i}'))
+            col_scaling.append(sg.InputText(size=(18), font=('Times New Roman', 15), key=f'scaling_{i}'))
         
         # Settings window layout
         settings_layout = [
             col_option,
-            col_type, 
+            col_type,
             col_scaling,
             [sg.Button("Save"), sg.Button("Cancel")],
         ]
+
         
         # Create the settings window
         settings_window = sg.Window("Settings", settings_layout, modal=True)
@@ -180,33 +274,45 @@ while True:
         # Hide the main window
         window.hide()
         
-        # Event loop for the settings window
+        # Infinite loop until the user cancels or saves settings
         while True:
+            # Initialize lists to store user options, types, and scalings
             user_options = []
             user_types = []
             user_scalings = []
+            
+            # Read settings from the settings window
             settings_event, settings_values = settings_window.read()
             
+            # Break the loop if the settings window is closed or canceled
             if settings_event == sg.WINDOW_CLOSED or settings_event == "Cancel":
                 break
             
+            # Check if the user clicked the "Save" button
             if settings_event == "Save":
-                for i in range(num_options): 
-                    if 0 <= float(settings_values[f'scaling_{i}']) <= 1 and settings_values[f'type_{i}'] in ['pwd', 'mag', 'plx', 'distance', 'teff', 'pm', 'spt']: 
+                # Iterate over each option
+                for i in range(num_options):
+                    # Check if the scaling value is between 0 and 1 and if the type is valid
+                    if 0 <= float(settings_values[f'scaling_{i}']) <= 1 and settings_values[f'type_{i}'] in ['pwd', 'mag', 'plx', 'distance', 'teff', 'pm']:
+                        # If valid, append user options, types, and scalings
                         user_options.append(settings_values[f'option_{i}'])
                         user_types.append(settings_values[f'type_{i}'])
-                        user_scalings.append(settings_values[f'scaling_{i}']) 
-                    else: 
+                        user_scalings.append(settings_values[f'scaling_{i}'])
+                    else:
+                        # If settings are incorrect, notify the user
                         print('#------------------------------------------------#')
                         print('#         Please Input Correct Settings!         #')
                         print('#------------------------------------------------#')
-                if int(len(user_scalings)) == int(num_options) and int(len(user_options)) == int(num_options) and int(len(user_types)) == int(num_options): 
+                
+                # Check if all lists have the correct length
+                if len(user_scalings) == num_options and len(user_options) == num_options and len(user_types) == num_options:
+                    # Break the loop if settings are valid
                     break
-        
-        settings_window.close()
-        
-        # Show the main window again
-        window.un_hide()
+                
+                settings_window.close()
+                
+                # Show the main window again
+                window.un_hide()
     
     if event in (None, 'Run'):
         try: 
@@ -236,11 +342,24 @@ while True:
             print('#----------------------------------------------------#')
             
             table = table_read()
-            column_space = column_read(table, user_options, user_types)
-            parameter_matrix = parm_metrix(column_space, user_types)
-            ranked_list = ranking_mult(parameter_matrix, user_scalings)
-            ranked_table = rank_table(table, ranked_list)
-            rank_table_save(ranked_table, output)
+            if (table == 0).all().all(): 
+                print('#------------------------------------------------#')
+                print('#         Please Enter a Correct Table!          #')
+                print('#------------------------------------------------#')
+                pass
+            else:
+                column_space = column_read(table, user_options, user_types)
+                if column_space == 0: 
+                    print('#------------------------------------------------#')
+                    print('#        Please Enter a Correct Setting!         #')
+                    print('#------------------------------------------------#')
+                    pass
+                else: 
+                    parameter_matrix = parm_metrix(column_space, user_types)
+                    ranked_list = ranking_mult(parameter_matrix, user_scalings)
+                    ranked_table = rank_table(table, ranked_list)
+                    rank_table_save(ranked_table, output)
+                
             
             print('#----------------------------------------------------#')
             print('#           ExoRANK Has Finished Running!            #')
@@ -265,6 +384,9 @@ while True:
         print('               Closing ExoRANK                       ')
         print('#------------------------------------------------#')
         break
+# ------------------------------------------------------------- #
+
+
 
 #Closes the window
 window.close()
---
+++ README.md
@@ -0,0 +1,120 @@
+<h1 align="center" id="title"> 📉 ExoRANK 📈 </h1>
+<div align="center">
+
+<p id="description"> <b>ExoRANK</b> is a tool designed to rank data within a table based on specified input parameters, which are derived from the column names in the table. The supported input parameters include probability of a white dwarf, magnitude space, parallax, distance, effective temperature, and proper motion. The program processes the data and outputs the original CSV file with an additional column labeled "Rank".  </p>
+</div>
+
+<div align="center">
+  <h2>🛠️ Installation 🛠️</h2>
+</div>
+
+<div align="center">
+<pp><b>🐍 Anaconda Environment 🐍</b><pp>
+</div>
+<div align="center">
+<pp><b>-----------------------------------------</b><pp>
+</div>
+
+**Note**: Making use of an Anaconda (Conda) environment is highly recommended due to potential compatibility issues with older Python and pip packages when using ExoRANK.
+
+1. **Download Anaconda Navigator**: To get started, please visit [Anaconda's official website](https://www.anaconda.com) and download the version of Anaconda Navigator that matches your operating system.
+
+2. **Access Anaconda Environments**: After downloading and installing Anaconda Navigator, launch the application. In the Navigator's main window, you will find an "Environments" option on the left. Click on it to access your Conda environments.
+
+3. **Create a New Conda Environment**: In the "Environments" section, you will see a plus sign labeled "Create." Click on it to create a new Conda environment.
+
+4. **Customize Environment Settings**: Give your new environment a name of your choice (e.g., "exorank"). Next, select a Python version that is closest to Python 3.8 (e.g., 3.8.18). Then, click the "Create" button to create the environment.
+
+5. **Environment Creation**: The environment creation process may take a while, as Conda will install the necessary packages and dependencies for WRAP.
+
+6. **Activate Your Conda Environment**: Once the environment is successfully created, you can activate it in your terminal. Open your terminal and enter the following command, replacing `*conda name*` with the name you chose for your environment:
+   ```bash
+   conda activate *conda name*
+
+<div align="center">
+  <p><b>⬇️ PIP Installation ⬇️</b></p>
+</div>
+<div align="center">
+<pp><b>-----------------------------------------</b><pp>
+</div>
+
+1. Once the conda environment is activated, go to your directory containing the ExoRANK contents (e.g., "cd Documents/GitHub/ExoRANK").
+2. Run the command:
+   ```bash
+   pip install -r requirements.txt
+
+
+<div align="center">
+<pp><b> Opening ExoRANK </b><pp>
+</div>
+<div align="center">
+<pp><b>-----------------------------------------</b><pp>
+</div>
+
+To open the application, type the following command into your terminal:
+
+**Note that the "python" command might change on how python is installed on your machine**
+
+- For MacOS: `python ExoRANK.py`
+- For Windows: `python .\ExoRANK.py`
+
+<div align="center">
+  <h2>🏆 Using ExoRANK 🏆</h2>
+</div>
+
+<div align="center">
+  <p><b>How to Use WRAP</b></p>
+</div>
+<div align="center">
+<pp><b>-----------------------------------------</b><pp>
+</div>
+
+**ExoRANK Main Window**
+
+1. Open ExoRANK. You will need to provide five inputs before clicking "Run":
+2. Click "File Browser" to select your table. Supported table types are CSV, FITS, ASCII, and IPAC.
+3. Choose the file type of your selected table from the dropdown menu below.
+4. Enter the number of parameters by typing a positive integer in the "NOP" text box. This represents the columns you are using in the ranking algorithm.
+5. Type the desired output file name as a string in the "Output File Name" text box.
+6. Click the "Change Settings" button at the bottom. A new window will appear. Refer to the instructions below for guidance on using this popup window.
+7. Once all parameters are set, click "Run" to execute the algorithm.
+8. The output table will be saved as a CSV file in the "Output" file directory.
+
+**ExoRANK "Change Settings" Window**
+
+1. Once you click the "Change Settings" button, a new window will appear with N columns (equal to the number of parameters entered earlier). Each column corresponds to a different parameter and contains three rows requiring input.
+2. In the first row, labeled "Column Name," enter the name of the column to be used as a parameter.
+3. The second row, labeled "Column Type," features a dropdown menu with the following options:
+    * ***pwd***: Probability of White Dwarf (range: 0 to 1)
+    * ***mag***: Magnitude of a particular band
+    * ***plx***: Parallax of an object in milliarcseconds
+    * ***distance***: Distance of an object in parsecs
+    * ***teff***: Effective temperature of the white dwarf in Kelvin
+    * ***pm***: Proper motion of the white dwarf in milliarcseconds per year
+1. The third row, labeled "Column Scaling," requires you to input a scaling value for each parameter, ranging from 0 to 1.
+2. After all fields are completed, click "Save" to return to the main window in ExoRANK.
+
+<div align="center">
+  <p><b>⚠️ Significant Details ⚠️</b></p>
+  <p>-----------------------------------------</p>
+</div>
+
+- **Note 1**: Only tested on MacOS >11; problems may occur for older versions of MacOS and Windows ***(NOT SUPPORTED ON LINUX)***.
+- **Note 2**: The window close button has been disabled; to close ExoRANK, please click the red "Close" button at the bottom.
+
+
+<div align="center">
+  <h2>📞 Support 📞</h2>
+</div>
+
+- **Mr. Hunter Brooks**
+  - Email: hcb98@nau.edu
+
+<div align="center">
+  <h2>📖 Acknowledgments 📖</h2>
+</div>
+
+1. If you intend to publish any of the data gathered by WRAP, please ensure that you correctly acknowledge the sources of the data. 
+
+2. In addition to using the correct acknowledgments for each catalog, please cite [Brook et al. (2023)](https://ui.adsabs.harvard.edu/abs/2023RNAAS...7..272B/abstract) when using WRAP for any publication.
+
